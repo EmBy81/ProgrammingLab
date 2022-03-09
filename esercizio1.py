5e28843719dc33745b2be1d6210d89c14b07e128
Emby81 <101270500+EmBy81@users.noreply.github.com>
2022-03-09 16:55:11 +0000

    somma di numeri

---
+++ esercizio1.py
@@ -0,0 +1,8 @@
+a=int(input("num A = "))
+b=int(input("num B = "))
+c=int(input("num C = "))
+def somma(a,b,c):
+    result = a+b+c
+    return result
+
+print("A+B+C = {} " .format(somma(a,b,c)))
---
+++ hello.py
@@ -0,0 +1,2 @@
+print('Hello world!')
+#print('valore 1: {}'.format(my_var))
