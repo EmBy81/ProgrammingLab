510b626df379d76ca1cfc875e3ded20040b37411
Emby81 <101270500+EmBy81@users.noreply.github.com>
2022-03-23 10:08:19 +0000

    di nuovo per autograding

--- esercizio3.py
+++ esercizio3.py
@@ -10,17 +10,17 @@ class CSVfile():
             elements = line.split(',')
             elements[-1] = elements[-1].strip()
             if elements[0] != 'Date':
-                date = elements[0]
-                value = elements[1]
+                #date = elements[0]
+                #value = elements[1]
                 #values.append(date)
                 #values.append(value)
-                values.append([date,value])                
-                #values.append(elements)
+                #values.append([date,value])                
+                values.append(elements)
         my_file.close()
         return values
 
-#csvfile = Csvfile('ciao.txt')
+# csvfile = Csvfile('ciao.txt')
 # per far passare, commento anche quei due sotto
-#csvfile = CSVfile('shampoo_sales.csv')
-#print(csvfile.get_data())
+# csvfile = CSVfile('shampoo_sales.csv')
+# print(csvfile.get_data())
 
